299278e7c1a0bf55b8ec460ccbd48efb1f45bcd3
mkbaker <mattbakerk@gmail.com>
2016-11-27 14:57:33 -0600

    [Re #4] Created WaveSpawner script.

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
