c9b443a0332cd601f0a02f02e64db65d22fce715
mkbaker <mattbakerk@gmail.com>
2016-11-26 22:41:25 -0600

    [Re #3] Added target and transform objects.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,4 +5,16 @@ public class Enemy : MonoBehaviour {
     public float speed = 10f;
 
     private Transform target;
+    private int waypointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir);
+    }
 }
