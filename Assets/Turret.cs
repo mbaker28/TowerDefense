dd090a492eb36412c1f03af4c1c35106ad69eed9
mkbaker <mattbakerk@gmail.com>
2016-11-27 21:05:50 -0600

    [Re #5] Configured lock-on script.

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
-    public Transform target;
+    private Transform target;
     public float range = 15f;
 
     public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
 
 	// Use this for initialization
 	void Start () {
@@ -33,6 +36,10 @@ public class Turret : MonoBehaviour {
         {
             target = nearestEnemy.transform;
         }
+        else
+        {
+            target = null;
+        }
     }
 	
 	// Update is called once per frame
@@ -41,6 +48,12 @@ public class Turret : MonoBehaviour {
         {
             return;
         }
+
+        //Target lock on
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
 	}
 
     void OnDrawGizmosSelected()
