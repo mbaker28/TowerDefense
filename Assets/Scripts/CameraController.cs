9925f93e16d3e953cae4fa800c924f00efa99221
mkbaker <mattbakerk@gmail.com>
2016-11-28 14:38:34 -0600

    [Re #8] Implemented panning and scrolling tools. Added scroll limit so user cannot scroll too far down or up.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,11 +2,25 @@
 
 public class CameraController : MonoBehaviour {
 
+    private bool doMovement = true;
+
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            doMovement = !doMovement;
+
+        if (!doMovement)
+        {
+            return;
+        }
 	    
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
@@ -24,5 +38,14 @@ public class CameraController : MonoBehaviour {
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
         }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
     }
 }
