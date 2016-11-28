b6e6b227203e4abd05fc4daf1362fb713021208d
mkbaker <mattbakerk@gmail.com>
2016-11-27 20:48:02 -0600

    [Re #5] Added targetting scripts to Turret.cs

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
+    public Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
 	// Use this for initialization
 	void Start () {
-	
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
 	}
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (target == null)
+        {
+            return;
+        }
 	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
 }
